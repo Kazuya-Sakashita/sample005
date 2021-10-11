39556334752fb4f97aa24d86648b7ec23436894f
Kazuya-Sakashita <kz0508@gmail.com>
2021-10-11 19:42:25 +0900

    add-rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,16 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:github]
 
-   def self.from_omniauth(access_token)
+  def self.from_omniauth(access_token)
     data = access_token.info
     user = User.where(email: data['email']).first
-    unless user
-        user = User.create(
-           email: data['email'],
-           password: Devise.friendly_token[0,20]
-        )
-    end
+    user ||= User.create(
+      email: data['email'],
+      password: Devise.friendly_token[0, 20]
+    )
     user
   end
-      
 end
--- test/controllers/managements_controller_test.rb
+++ test/controllers/managements_controller_test.rb
@@ -1,7 +1,7 @@
-require "test_helper"
+require 'test_helper'
 
 class ManagementsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get managements_index_url
     assert_response :success
   end
