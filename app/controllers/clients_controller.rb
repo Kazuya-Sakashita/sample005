9e4551f35dbb8725f7ae42f1074e7e318f817ce2
Kazuya-Sakashita <kz0508@gmail.com>
2021-10-16 19:42:01 +0900

    client一覧、登録、修正実装

---
+++ app/controllers/clients_controller.rb
@@ -0,0 +1,44 @@
+class ClientsController < ApplicationController
+  def index
+    @clients = Client.all
+  end
+
+  def new
+    @client = Client.new
+  end
+
+  def create
+    @client = Client.new(client_params)
+    if @client.save
+        flash[:notice] = '投稿しました！'
+        redirect_to clients_path
+      else
+        flash.now[:alert] = '入力内容が正しくありません'
+        render :index
+      end
+  end
+
+  def edit
+    @client = Client.find(params[:id])
+  end
+
+  def show
+  end
+
+  def update
+    @client = Client.find(params[:id])
+    @client.update(client_params)
+    redirect_to clients_path
+  end
+
+  def destroy
+  end
+
+
+  private
+
+  def client_params
+    params.require(:client).permit(:company_name, :staff, :email, :phone)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -20,5 +20,7 @@ Rails.application.routes.draw do
   resource :mypages
   resources :managements
   resources :users
+  resources :clients
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
